a81794ec14069bb58f759c7e30cba6c65a815477
WizMe-M <timkin.moxim@mail.ru>
2023-08-15 18:14:19 +0300

    create new order (api endpoint)

---
+++ Terk.API.Contracts/PostBodies/NewOrder.cs
@@ -0,0 +1,14 @@
+﻿namespace Terk.API.Contracts.PostBodies;
+
+/// <summary>
+/// New order created by user
+/// </summary>
+/// <param name="Positions">Positions of new order</param>
+public record NewOrder(NewOrderPosition[] Positions);
+
+/// <summary>
+/// Position in new order
+/// </summary>
+/// <param name="Id">ID of product</param>
+/// <param name="Count">Count of products</param>
+public record NewOrderPosition(int Id, byte Count);
--- Terk.API/Controllers/OrderController.cs
+++ Terk.API/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
-﻿using Order = Terk.API.Contracts.Responses.Order;
+﻿using Terk.API.Contracts.PostBodies;
+using Order = Terk.API.Contracts.Responses.Order;
+using Position = Terk.API.Contracts.Responses.Position;
 using Product = Terk.API.Contracts.Responses.Product;
 
 namespace Terk.API.Controllers;
@@ -17,8 +19,7 @@ public class OrderController : DbController
     /// Requests user's orders
     /// </summary>
     /// <param name="authorization">JWT Bearer authorization token</param>
-    /// <returns>Ok with array of <see cref="Order"/> either <see cref="ProblemDetails"/>
-    /// if user can't be authorized through jwt token</returns>
+    /// <returns>Ok with array of <see cref="Order"/> either <see cref="ProblemDetails"/> with message</returns>
     [HttpGet("my")]
     public async Task<IActionResult> GetMyOrders([FromHeader] string authorization)
     {
@@ -32,6 +33,7 @@ public class OrderController : DbController
         var orders = await Context.Orders
             .Include(order => order.OrderPositions)
             .Where(order => order.CustomerId == id)
+            .OrderByDescending(order => order.CreatedDate)
             .Select(order => new Order(
                 order.Id,
                 order.CreatedDate,
@@ -44,9 +46,54 @@ public class OrderController : DbController
                     position.ProductCount,
                     position.Cost)
                 ).ToArray(),
-                order.TotalCost)
-            ).ToArrayAsync();
+                order.TotalCost))
+            .ToArrayAsync();
         _logger.LogInformation("User (id={Id}) requested his orders (count={Count})", id, orders.Length);
         return Ok(orders);
+    }
+
+    /// <summary>
+    /// Creates new order for user
+    /// </summary>
+    /// <param name="authorization">User's authorization token</param>
+    /// <param name="newOrder">Data for new order</param>
+    /// <returns>Ok either <see cref="ProblemDetails"/></returns>
+    [HttpPost("new")]
+    public async Task<IActionResult> CreateNewOrder([FromHeader] string authorization, NewOrder newOrder)
+    {
+        var id = authorization.ExtractId();
+        if (id is null)
+        {
+            _logger.LogWarning("Unable to extract id from JWT: {Jwt}", authorization);
+            return Problem("Incorrect authorization token", statusCode: StatusCodes.Status400BadRequest);
+        }
+
+        // select products by ID
+        var productCosts =
+            Context.Products.Where(product => newOrder.Positions
+                .Select(nop => nop.Id)
+                .Contains(product.Id)
+            ).ToArray();
+
+        // create order's positions
+        var positions = newOrder.Positions.Select(nop => new OrderPosition
+        {
+            ProductId = nop.Id,
+            ProductCount = nop.Count,
+            Cost = (decimal)productCosts.First(productCost => productCost.Id == nop.Id).Cost * nop.Count,
+        }).ToArray();
+
+        // create order for user
+        var order = new Terk.DB.Entities.Order
+        {
+            CustomerId = id.Value,
+            OrderPositions = positions,
+            TotalCost = positions.Select(position => position.Cost).Sum(),
+        };
+
+        Context.Orders.Add(order);
+        await Context.SaveChangesAsync();
+
+        return Ok("Order successfully created");
     }
 }
